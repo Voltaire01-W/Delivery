261dcf5cbadd36b815dde7b89c3befeda1bcc214
Voltaire01-W <59457919+Voltaire01-W@users.noreply.github.com>
2021-05-25 08:59:17 -0500

    Added back button to restaurants screen

--- screens/Restaurant.js
+++ screens/Restaurant.js
@@ -1,15 +1,66 @@
-import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import React, { useState, useEffect } from 'react';
 import {
     View,
-    Text
+    Text,
+    StyleSheet,
+    SafeAreaView,
+    TouchableOpacity,
+    Image,
+    Animated
 } from 'react-native';
+import { isIphoneX } from 'react-native-iphone-x-helper';
+import { icons, COLORS, SIZES, FONTS } from '../constants';
 
-const Restaurant = () => {
+const Restaurant = ({ route, navigation }) => {
+
+    const [restaurant, setRestaurant] = useState(null);
+    const [currentLocation, setCurrentLocation] = useState(null);
+
+    useEffect(() => {
+        let { item, currentLocation } = route.params;
+
+        setRestaurant(item)
+        setCurrentLocation(currentLocation)
+    })
+
+    const renderHeader = () => {
+        return (
+            <View style={{ flexDirection: 'row' }}>
+                <TouchableOpacity
+                    style={{
+                        width: 50,
+                        height: 50,
+                        paddingLeft: SIZES.padding * 2,
+                        justifyContent: 'center'
+                    }}
+                    onPress={() => navigation.goBack()}
+                >
+                    <Image 
+                        source={icons.back}
+                        resizeMode="contain"
+                        style={{
+                            width: 30,
+                            height: 30
+                        }}
+                    />
+                </TouchableOpacity>
+            </View>
+        )
+    }
+
     return (
-        <View>
-            <Text>Restaurant</Text>
-        </View>
+        <SafeAreaView style={styles.container}>
+            {renderHeader()}
+        </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: COLORS.lightGray2
+    }
+})
 
 export default Restaurant
