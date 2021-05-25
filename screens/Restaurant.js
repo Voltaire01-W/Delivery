d549c50a65a6903d4638e3803f7062238b22e0df
Voltaire01-W <59457919+Voltaire01-W@users.noreply.github.com>
2021-05-25 09:19:30 -0500

    Completed Restaurant screen header

--- screens/Restaurant.js
+++ screens/Restaurant.js
@@ -45,6 +45,45 @@ const Restaurant = ({ route, navigation }) => {
                         }}
                     />
                 </TouchableOpacity>
+
+                {/* Restaurant Name Section */}
+                <View 
+                    style={{
+                        flex: 1,
+                        alignItems: 'center',
+                        justifyContent: 'center'
+                    }}
+                >
+                    <View
+                        style={{
+                            height: 50,
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            paddingHorizontal: SIZES.padding * 3,
+                            borderRadius: SIZES.radius,
+                            backgroundColor: COLORS.lightGray3
+                        }}
+                    >
+                        <Text style={{ ...FONTS.h3 }}>{restaurant?.name}</Text>
+                    </View>
+                </View>
+
+                <TouchableOpacity
+                    style={{
+                        width: 50,
+                        paddingRight: SIZES.padding * 2,
+                        justifyContent: 'center',
+                    }}
+                >
+                    <Image 
+                        source={icons.list}
+                        resizeMode="contain"
+                        style={{
+                            width: 30,
+                            height: 30
+                        }}
+                    />
+                </TouchableOpacity>
             </View>
         )
     }
